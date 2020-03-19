99d3b2d4118c6353ddf747524ccca003d03d8f84
srishtisuri <42334519+srishtisuri@users.noreply.github.com>
2020-03-19 20:44:39 +1100

    Add files via upload

--- make10.js
+++ make10.js
@@ -13,20 +13,50 @@ function getNumber(id) {
   return document.getElementById(id).value;
 }
 
-function calculate() {
+function arrangeOperations() {
   let operations = ["+", "-", "/", "*"];
-  let numbers = [num1, num2, num3, num4];
-  let tempArray_i = [];
 
   for (let i = 0; i < 4; i++) {
     for (let j = 0; j < 4; j++) {
       for (let k = 0; k < 4; k++) {
-        let tempArray_i = [];
-        tempArray_i.push(operations[i]);
-        tempArray_i.push(operations[j]);
-        tempArray_i.push(operations[k]);
-        console.log(`Array: ${tempArray_i}`);
+        tempArray_x.push(operations[i]);
+        tempArray_x.push(operations[j]);
+        tempArray_x.push(operations[k]);
+        console.log(`Array: ${tempArray_x}`);
       }
     }
   }
+}
+
+function arrangeNumbers() {
+  let numbers = ["1", "2", "3", "4"];
+  var numArrangements = [];
+  var tempArray_y = [];
+
+  // for (let i = 0; i < numbers.length ; i++) {
+  //   let array = [...numbers];
+  //   tempArray_y(array[i])
+  //   array.splice(i, 1)
+  //   for (let j = 0; j < array.length; j++) {
+  //     tempArray_y(array[j])
+  //     array.splice()
+  //   }
+  // }
+
+  for (let i = 0; i < 4; i++) {
+    tempArray_y.push(numbers[i]);
+    for (let j = 1; j < 4; j++) {
+      tempArray_y.push(numbers[j]);
+      for (let k = 2; k < 4; k++) {
+        tempArray_y.push(numbers[k]);
+        for (let l = 3; l < 4; l++) {
+          tempArray_y.push(numbers[l]);
+          numArrangements.push(tempArray_y);
+          //console.log(`Array: ${tempArray_y}`);
+        }
+        
+      } 
+    }
+  }
+  console.log(`Array: ${numArrangements}`)
 }
