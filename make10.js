33a078d48a47a33419fe1eb87c575f400a50bbbf
Srishti Suri <srishti301@gmail.com>
2020-03-24 14:04:01 +1100

    generate correct combinations of the numbers

--- make10.js
+++ make10.js
@@ -1,62 +1,89 @@
+//Global variables
 let num1 = getNumber("box1");
 let num2 = getNumber("box2");
 let num3 = getNumber("box3");
 let num4 = getNumber("box4");
 
+let numbers = ["4", "4", "4", "3"];
+
+let numArrangements = [];
+let tempArray = [];
+let finalArrangements = [];
+let operationsArrangements = [];
+
+//Move cursor for input boxes
 function moveOnMax(field, nextFieldID) {
   if (field.value.length >= field.maxLength) {
     document.getElementById(nextFieldID).focus();
   }
 }
 
+//Retrieve numbers from input
 function getNumber(id) {
   return document.getElementById(id).value;
 }
 
+//All possible combinations of any 3 of the 4 maths operations
 function arrangeOperations() {
   let operations = ["+", "-", "/", "*"];
 
   for (let i = 0; i < 4; i++) {
     for (let j = 0; j < 4; j++) {
       for (let k = 0; k < 4; k++) {
-        tempArray_x.push(operations[i]);
-        tempArray_x.push(operations[j]);
-        tempArray_x.push(operations[k]);
-        console.log(`Array: ${tempArray_x}`);
+        operationsArrangements.push([
+          operations[i],
+          operations[j],
+          operations[k]
+        ]);
+      }
+    }
+  }
+  console.log(operationsArrangements);
+  //return operationsArrangements;
+}
+
+//All possible combinations of the 4 numbers
+function arrangeNumbers() {
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
+      for (let k = 0; k < 4; k++) {
+        for (let l = 0; l < 4; l++) {
+          numArrangements.push([
+            numbers[i],
+            numbers[j],
+            numbers[k],
+            numbers[l]
+          ]);
+
+          tempArray.push([numbers[i], numbers[j], numbers[k], numbers[l]]);
+        }
       }
     }
   }
 }
 
-function arrangeNumbers() {
-  let numbers = ["1", "2", "3", "4"];
-  var numArrangements = [];
-  var tempArray_y = [];
-
-  // for (let i = 0; i < numbers.length ; i++) {
-  //   let array = [...numbers];
-  //   tempArray_y(array[i])
-  //   array.splice(i, 1)
-  //   for (let j = 0; j < array.length; j++) {
-  //     tempArray_y(array[j])
-  //     array.splice()
-  //   }
-  // }
-
-  for (let i = 0; i < 4; i++) {
-    tempArray_y.push(numbers[i]);
-    for (let j = 1; j < 4; j++) {
-      tempArray_y.push(numbers[j]);
-      for (let k = 2; k < 4; k++) {
-        tempArray_y.push(numbers[k]);
-        for (let l = 3; l < 4; l++) {
-          tempArray_y.push(numbers[l]);
-          numArrangements.push(tempArray_y);
-          //console.log(`Array: ${tempArray_y}`);
-        }
-        
-      } 
+//Check duplicate combinations in the final array
+function checkIncludes(index) {
+  let includes = false;
+  finalArrangements.forEach(arr => {
+    if (arr.toString() == numArrangements[index].toString()) {
+      includes = true;
     }
-  }
-  console.log(`Array: ${numArrangements}`)
+  });
+  return includes;
+}
+
+//Remove the combinations with any repetitions
+function removeArraysWithDuplicates() {
+  arrangeNumbers();
+
+  tempArray.filter((arr, index) => {
+    if (
+      arr.sort().toString() == numbers.sort().toString() &&
+      !checkIncludes(index)
+    ) {
+      finalArrangements.push(numArrangements[index]);
+    }
+  });
+  console.log(finalArrangements);
 }
